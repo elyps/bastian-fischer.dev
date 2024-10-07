ebe3b6470a8debe004e5a68d07362a08060598fe
Bastian Fischer <kontakt@bastian-fischer.dev>
2024-10-07 13:44:57 +0200

    Create about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,20 @@
-// src/pages/index.js
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-import React from "react";
+// Step 2: Define your component
+const IndexPage = () => {
+  return (
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+	  <Link to="/about">About</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
+}
 
-const UnderConstruction = () => (
-  <div style={styles.container}>
-    <h1 style={styles.heading}>Diese Seite ist momentan im Umbau!</h1>
-    <p style={styles.paragraph}>
-      Wir arbeiten fleißig daran, unsere Website zu verbessern. Schau bald wieder vorbei!
-    </p>
-  </div>
-);
+// You'll learn about this in the next task, just copy it for now
+export const Head = () => <title>Home Page</title>
 
-export const Head = () => <title>Under Construction</title>
-
-const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    minHeight: '100vh',
-    backgroundColor: '#f7f7f7',
-    textAlign: 'center',
-    padding: '20px',
-  },
-  heading: {
-    fontSize: '3rem',
-    color: '#333',
-  },
-  paragraph: {
-    fontSize: '1.5rem',
-    color: '#555',
-    marginBottom: '20px',
-  },
-  image: {
-    maxWidth: '100%',
-    height: 'auto',
-  },
-};
-
-export default UnderConstruction;
+// Step 3: Export your component
+export default IndexPage
