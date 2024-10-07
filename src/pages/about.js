f8154776844fae4ef5368ac992595816ade82753
Bastian Fischer <kontakt@bastian-fischer.dev>
2024-10-07 15:23:35 +0200

    Add layout module

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,13 @@
 // Step 1: Import React
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from './layout'
 
 // Step 2: Define your component
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Me</h1>
-	  <Link to="/">Back to Home</Link>
+    <Layout pageTitle="About Page">
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
-    </main>
+    </Layout>
   )
 }
 
