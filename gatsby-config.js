17fb674572074642aa16b9916316f76d1e1602d9
Bastian Fischer <kontakt@bastian-fischer.dev>
2024-10-07 23:43:33 +0200

    Added cover images to posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
 			resolve: "gatsby-source-filesystem",
 			options: {
 				name: `blog`,
-				path: `${__dirname}/blog`,
+				path: `${__dirname}/src/pages/blog`,
 			}
 		},
 		"gatsby-plugin-mdx",
--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -2,22 +2,43 @@ import * as React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const BlogPost = ({ data, children }) => {
-  return (
-    <Layout pageTitle={data.mdx.frontmatter.title}>
-      <p>{data.mdx.frontmatter.date}</p>
-      {children}
-    </Layout>
-  )
+	const image = getImage(data.mdx.frontmatter.hero_image)
+
+	return (
+		<Layout pageTitle={data.mdx.frontmatter.title}>
+			<p>{data.mdx.frontmatter.date}</p>
+			<GatsbyImage
+				image={image}
+				alt={data.mdx.frontmatter.hero_image_alt}
+			/>
+			<p>
+				Photo Credit:{" "}
+				<a href={data.mdx.frontmatter.hero_image_credit_link}>
+					{data.mdx.frontmatter.hero_image_credit_text}
+				</a>
+			</p>
+			{children}
+		</Layout>
+	)
 }
 
 export const query = graphql`
-  query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
     }
   }
